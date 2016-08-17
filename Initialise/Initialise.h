87641383bcee527aa2d224409b54b59258da73ec
Josh Campion <joshcampion89@gmail.com>
2016-08-17 12:23:56 +0100

    Initial Commit

---
+++ Initialise/Initialise.h
@@ -0,0 +1,19 @@
+//
+//  Initialise.h
+//  Initialise
+//
+//  Created by Josh Campion on 17/08/2016.
+//  Copyright © 2016 Josh Campion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Initialise.
+FOUNDATION_EXPORT double InitialiseVersionNumber;
+
+//! Project version string for Initialise.
+FOUNDATION_EXPORT const unsigned char InitialiseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Initialise/PublicHeader.h>
+
+
